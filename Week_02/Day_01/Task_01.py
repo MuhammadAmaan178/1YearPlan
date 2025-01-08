f99e1d76edffb04061b39b5f5ba99691849e49e8
Muhammad Amaan <mohammadamaanc178@gmail.com>
2025-01-08 19:16:07 +0500

    Delete Week_02/Day_01 directory

--- Week_02/Day_01/Task_01.py
+++
@@ -1,3 +0,0 @@
-#Write a program to print your name and age using print().
-print("Amaan")
-print("18")
--- Week_02/Day_01/Task_03.py
+++
@@ -1,3 +0,0 @@
-# Write a program to print the current date using Python's datetime module.
-import datetime
-print(datetime.datetime.now())
