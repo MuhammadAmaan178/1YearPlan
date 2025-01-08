ce0ddd1fbfde5113e6ce999d17c1d8ce7c4703c2
Muhammad Amaan <mohammadamaanc178@gmail.com>
2025-01-08 19:17:50 +0500

    Add files via upload

---
+++ Week_02/Day_01/Task_01.py
@@ -0,0 +1,3 @@
+#Write a program to print your name and age using print().
+print("Amaan")
+print("18")
---
+++ Week_02/Day_01/Task_03.py
@@ -0,0 +1,3 @@
+# Write a program to print the current date using Python's datetime module.
+import datetime
+print(datetime.datetime.now())
