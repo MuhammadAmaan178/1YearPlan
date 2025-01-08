49c8df156ea1aa090d0eedead945c4bdbce7409a
Muhammad Amaan <mohammadamaanc178@gmail.com>
2025-01-08 19:15:16 +0500

    Add files via upload

---
+++ Week_02/Day_01/Task_02.py
@@ -0,0 +1,3 @@
+#Experiment with input() to accept a user’s favorite color and print a message including it.
+fav_color = input("Enter Your Favourite Colour")
+print("Your Favourite Colour Is ",fav_color)
