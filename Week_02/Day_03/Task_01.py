4c0b55efbbf51f4f26fe1d305fb3380e90de8799
Muhammad Amaan <mohammadamaanc178@gmail.com>
2025-01-10 21:14:32 +0500

    Add files via upload

---
+++ Week_02/Day_03/Task_01.py
@@ -0,0 +1,3 @@
+#Write a program to take a user's name and greet them with "Hello, [Name]!".
+name = input("Enter Your Name : ")
+print(f"Hello {name}")
---
+++ Week_02/Day_03/Task_02.py
@@ -0,0 +1,5 @@
+#Create a program to accept three numbers as input and calculate their average.
+n1 = float(input("Enter First Number : "))
+n2 = float(input("Enter Second Number : "))
+n3 = float(input("Enter Third Number : "))
+print(f"Average : {(n1+n2+n3)/3}")
